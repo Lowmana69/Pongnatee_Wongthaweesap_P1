c03d7b2d5b59207d0b935aa98947a7d41379e0c9
Pongnatee Wongthaweesap <Lowmana69@gmail.com>
2020-05-25 08:13:41 -0700

    Added the authentication package and some new files

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+/* Exporting Keys to be used */
+
+exports.company = {
+    employee: process.env.COMPANY_EMPLOYEE,
+    manager: process.env.COMPANY_FINANCE_MANAGER,
+    supervisor: process.env.COMPANY_SUPERVISOR,
+    auditor: process.env.COMPANY_AUDITOR,
+    human_resources: process.env.COMPANY_HUMAN_RESOURCES,
+    portal: process.env.COMPANY_ACCESS_PORTAL,
+    primary: process.env.COMPANY_ACCESS_PRIMARY
+};
---
+++ config/passport.js
@@ -0,0 +1,6 @@
+/* Import Modules */
+
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import keys from './keys';
+
