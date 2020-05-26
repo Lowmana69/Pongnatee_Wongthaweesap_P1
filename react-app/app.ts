f8399f7689b52df4a7fde0b57a4c4a1ce0c5977a
Pongnatee Wongthaweesap <Lowmana69@gmail.com>
2020-05-26 16:34:13 -0700

    Deleted the old files while updating the new React files to match up. Also moved everything into the react-app Folder to be the main folder

---
+++ react-app/app.ts
@@ -0,0 +1,10 @@
+/* Import Modules */
+
+import express from 'express';
+import passport from 'passport';
+import session from 'express-session';
+
+/* Import Files */
+
+import keys from './config/keys';
+import passportSetup from '';
