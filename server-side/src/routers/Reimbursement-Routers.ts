232c4e923cdba39dbdc7c82197551fce46e7e578
Pongnatee Wongthaweesap <Lowmana69@gmail.com>
2020-06-05 14:46:22 -0700

    Finish the other Router File and now will try and finish the data and auth

--- server-side/src/routers/Reimbursement-Routers.ts
+++ server-side/src/routers/Reimbursement-Routers.ts
@@ -4,15 +4,43 @@ import express from 'express';
 
 /* Import Files */
 
-import * as reimbursementService from '../services/Reimbursement-Services';
+import * as reimbursementsService from '../services/Reimbursement-Services';
 
-export const reimbursementRouter = express.Router();
+/* Export Reimbursement Router */
+
+export const reimbursementsRouter = express.Router();
 
 /* =============== READ / RETRIEVE ============= */
 
 /* GET */
 
+reimbursementsRouter.get('', async (request, response, next) => {
+    try {
+        const reimbursements = await reimbursementsService.getAllReimbursements();
+        response.json(reimbursements);
+    } catch (error) {
+        response.sendStatus(500);
+    }
+});
+
 /* GET */
+
+reimbursementsRouter.get('', async (request, response, next) => {
+    const reimbursementInfo = +request.params.author;
+    try {
+        const reimbursementByUserId = await reimbursementsService
+            .getAllReimbursementByUserID(reimbursementInfo);
+        if (!reimbursementByUserId) {
+            response.sendStatus(404);
+        } else {
+            response.json(reimbursementByUserId);
+        }
+        next();
+    } catch (error) {
+        response.sendStatus(500);
+        next();
+    }
+});
 
 /* GET */
 
@@ -22,6 +50,37 @@ export const reimbursementRouter = express.Router();
 
 /* POST */
 
+reimbursementsRouter.post('', async (request, response, next) => {
+    const reimbursmentInputs = request.body;
+    try {
+        const reimbursementInfo = await reimbursementsService
+            .createNewReimbursement(reimbursmentInputs);
+        response.status(201);
+        response.json(reimbursementInfo);
+        next();
+    } catch (error) {
+        response.sendStatus(500);
+        next();
+    }
+});
+
 /* ===================== UPDATE ================= */
 
 /* PATCH */
+
+reimbursementsRouter.patch('', async (request, response, next) => {
+    const reimbursementInput = request.body;
+    try {
+        const reimbursementUpdate = await reimbursementsService
+            .updateReimbursement(reimbursementInput);
+        if (reimbursementUpdate) {
+            response.json(reimbursementUpdate);
+        } else {
+            response.sendStatus(404);
+        }
+        next();
+    } catch (error) {
+        response.sendStatus(500);
+        next();
+    }
+});
